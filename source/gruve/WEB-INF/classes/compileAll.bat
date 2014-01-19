40ec9465ea8b811c8faff19b77bc5d9d6f7b5f9e
srrvnn <srrvnn@live.com>
2014-01-19 10:07:26 -0800

    added a new compile file

--- source/gruve/WEB-INF/classes/compileAll.bat
+++ source/gruve/WEB-INF/classes/compileAll.bat
@@ -1,1 +1,3 @@
+cd %~dp0 
 javac -Xlint:unchecked -cp "../lib/json_simple-1.1.jar;../lib/slf4j-api-1.6.2.jar;../lib/slf4j-log4j12-1.6.2.jar;." hw/macs/gruve/*.java
+javac -Xlint:unchecked -cp "C:\Program Files\Apache Software Foundation\Tomcat 8.0\lib\servlet-api.jar;../lib/json_simple-1.1.jar;." GruveServlet.java
---
+++ source/gruve/WEB-INF/classes/compileGruve.bat
@@ -0,0 +1,1 @@
+javac -Xlint:unchecked -cp "../lib/json_simple-1.1.jar;../lib/slf4j-api-1.6.2.jar;../lib/slf4j-log4j12-1.6.2.jar;." hw/macs/gruve/*.java
